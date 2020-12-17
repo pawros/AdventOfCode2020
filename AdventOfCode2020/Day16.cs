a1062a187867c2b358807f074985a48430c0fd7f
Paweł Rosłon (PAWR) <pawr@demant-technology.com>
2020-12-17 18:23:34 +0100

    Day 16

--- AdventOfCode2020/Day16.cs
+++ AdventOfCode2020/Day16.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -8,28 +9,50 @@ namespace AdventOfCode2020
 {
     public sealed class Day16 : BaseDay
     {
+        private class Field
+        {
+            public string Name { get; set; }
+            public (int a, int b)[] Ranges { get; set; }
+
+            public int Position { get; set; }
+            public Field()
+            {
+                Ranges = new (int a, int b)[2];
+            }
+        }
+        
         private class Ticket
         {
             public int[] Values { get; set; }
             public bool IsValid { get; set; }
         }
         
-        private readonly (int a, int b)[] ranges;
+        private readonly Field[] fields;
         private readonly int[] ticket;
         private readonly Ticket[] nearbyTickets;
+        
         public Day16()
         {
             var input = File.ReadAllText(InputFilePath).Split("\r\n\r\n");
 
             var fieldsInput = input[0].Split("\r\n");
-            ranges = new (int a, int b)[fieldsInput.Length * 2];
+            fields = new Field[fieldsInput.Length];
             for (var i = 0; i < fieldsInput.Length; i++)
             {
-                var line = fieldsInput[i];
-                var match = Regex.Match(line, @"^[\sa-z]*:\s([0-9]*)-([0-9]*)\sor\s([0-9]*)-([0-9]*)");
                 
-                ranges[2 * i] = (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
-                ranges[2 * i + 1] = (int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+                var line = fieldsInput[i];
+                var match = Regex.Match(line, @"^([\sa-z]*):\s([0-9]*)-([0-9]*)\sor\s([0-9]*)-([0-9]*)");
+
+                fields[i] = new Field
+                {
+                    Name = match.Groups[1].Value,
+                    Ranges =
+                    {
+                        [0] = (int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value)),
+                        [1] = (int.Parse(match.Groups[4].Value), int.Parse(match.Groups[5].Value))
+                    }
+                };
+
             }
 
             var ticketInput = input[1].Split("\r\n");
@@ -53,7 +76,7 @@ namespace AdventOfCode2020
             var errorRate = 0;
             foreach (var nearbyTicket in nearbyTickets)
             {
-                var ticketErrorRate = nearbyTicket.Values.Where(x => !ranges.Any(r => InRange(x, r))).Sum();
+                var ticketErrorRate = nearbyTicket.Values.Where(x => !fields.Any(f => f.Ranges.Any(r => InRange(x, r)))).Sum();
                 if (ticketErrorRate > 0)
                 {
                     nearbyTicket.IsValid = false;
@@ -62,25 +85,43 @@ namespace AdventOfCode2020
                 errorRate += ticketErrorRate;
             }
 
-            bool InRange(int n, (int a, int b) range)
-            {
-                return n >= range.a && n <= range.b;
-            }
-            
             return errorRate.ToString();
         }
-
+        
         public override string Solve_2()
         {
             var validTickets = nearbyTickets.Where(t => t.IsValid).ToArray();
             var positionValues = new Dictionary<int, int[]>(validTickets.Length);
             
-            for (var i = 0; i < validTickets.Length; i++)
+            for (var i = 0; i < ticket.Length; i++)
             {
                 positionValues[i] = validTickets.Select(t => t.Values[i]).ToArray();
             }
+
+            while (positionValues.Count > 0)
+            {
+                foreach (var field in fields)
+                {
+                    var matchedPositionValues = positionValues.Where(p => p.Value.All(x => field.Ranges.Any(r => InRange(x, r)))).ToArray();
+                    if (matchedPositionValues.Length == 1)
+                    {
+                        var position = matchedPositionValues.Single().Key;
+                        field.Position = position;
+                        positionValues.Remove(position);
+                    }
+                }
+            }
             
-            return string.Empty;
+            long result = fields
+                .Where(f => f.Name.StartsWith("departure"))
+                .Aggregate<Field, long>(1, (current, departureField) => current * ticket[departureField.Position]);
+
+            return result.ToString();
+        }
+        
+        private static bool InRange(int n, (int a, int b) range)
+        {
+            return n >= range.a && n <= range.b;
         }
     }
 }
